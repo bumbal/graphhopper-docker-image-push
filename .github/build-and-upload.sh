8984cae6271302bf838703214e060bfc80c7924e
Mohamed Chachia <83012233+medch1@users.noreply.github.com>
2024-05-23 11:56:15 +0200

    refractor build-and-upload.sh

--- .github/build-and-upload.sh
+++ .github/build-and-upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Buidling and pushing israelhikingmap/graphhopper:latest"
+echo "Buidling and pushing bumbal/graphhopper:latest"
 ./build.sh --push
 
 TAG=`cd graphhopper; git for-each-ref --sort=committerdate refs/tags | sed -n '$s/.*\///p'`
-if docker manifest inspect "israelhikingmap/graphhopper:${TAG}" >/dev/null; then 
+if docker manifest inspect "bumbal/graphhopper:${TAG}" >/dev/null; then 
     echo "No need to push existing version: ${TAG}";
 else
-    echo "Buidling and pushing israelhikingmap/graphhopper:${TAG}"
+    echo "Buidling and pushing bumbal/graphhopper:${TAG}"
     ./build.sh --push "${TAG}"
 fi
